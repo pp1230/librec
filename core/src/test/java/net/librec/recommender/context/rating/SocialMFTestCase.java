ae4786097700622d6cdd480ac37a60f0bedee301
pi <472220372@qq.com>
2017-07-07 09:34:14 +0800

    luo

--- core/src/test/java/net/librec/recommender/context/rating/SocialMFTestCase.java
+++ core/src/test/java/net/librec/recommender/context/rating/SocialMFTestCase.java
@@ -1,6 +1,14 @@
 /**
  * Copyright (C) 2016 LibRec
  *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * This file is part of LibRec.
  * LibRec is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
